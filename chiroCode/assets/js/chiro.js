ea49516c60471357513421163741005f5f6ace46
Krista Charner <kristacharner@outlook.com>
2017-04-27 11:03:48 -0400

    initial commit: new repo

---
+++ chiroCode/assets/js/chiro.js
@@ -0,0 +1,13 @@
+$('#palmSpringsMap').hide();
+
+
+function display() {
+	$('#carlsbadMap').hide();
+	$('#palmSpringsMap').show();
+}
+
+// $("#palmSpringsOffice").on("click", display());
+
+
+
+// $('#carlsbadMap').hide();
